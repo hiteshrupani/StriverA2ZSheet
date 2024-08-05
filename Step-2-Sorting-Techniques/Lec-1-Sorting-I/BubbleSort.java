db4393b1a5971b100942f17a2f3bacf32844125d
Hitesh Rupani <rupanihiteshabc@gmail.com>
2024-08-05 17:57:19 +0530

    2.1.2

---
+++ Step-2-Sorting-Techniques/Lec-1-Sorting-I/BubbleSort.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+//import java.io.*;
+
+class BubbleSort
+{
+    //method to print the Elements of the array
+    static void printArray(int[] arr)
+    {
+        int n = arr.length;
+        for (int j : arr) System.out.print(j + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args)
+    {
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        //taking total testcases
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            //taking total elements
+            int n = sc.nextInt();
+
+            //creating a new array of length n
+            int[] arr = new int[n];
+
+            //inserting elements to the array
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+
+
+            //calling bubbleSort() method
+            Solution12.bubbleSort(arr,n);
+
+            //calling printArray() method
+            printArray(arr);
+
+            t--;
+        }
+    }
+
+}
+// } Driver Code Ends
+
+//User function Template for Java
+
+class Solution12
+{
+    //Function to sort the array using bubble sort algorithm.
+    public static void bubbleSort(int[] arr, int n)
+    {
+        // Rounds
+        for (int i = n-1; i >= 1; i--) {
+
+            // Pushing max elem to last
+            for (int j = 0; j <= i-1; j++) {
+
+                // comparing adjacent elem
+                if (arr[j] > arr[j+1]) {
+
+                    // swapping
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
