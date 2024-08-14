620f60675b7a2b725022702326d3647d0c170c96
Hitesh Rupani <rupanihiteshabc@gmail.com>
2024-08-15 02:56:08 +0530

    3.1.1

---
+++ Step-3-Arrays/Lec-1-Easy/LargestElement.java
@@ -0,0 +1,62 @@
+// 3.1.1 -
+
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class LargestElement {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            int res = Solution311.largest(n, arr);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution311 {
+    public static int largest(int n, int[] arr) {
+        int largest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        return largest;
+    }
+}
